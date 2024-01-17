ffc937e17f0050624af67771ff795225da2b46ab
matthijsdam <matthijs0894@hotmail.com>
2024-01-17 15:40:36 +0100

    Setup firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import './app.css'
 import App from './App.svelte'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrHWZUMm56-BiXYE9-AkK55Y5Uk_6f5Zs",
+  authDomain: "openprio-80e7e.firebaseapp.com",
+  projectId: "openprio-80e7e",
+  storageBucket: "openprio-80e7e.appspot.com",
+  messagingSenderId: "953925241260",
+  appId: "1:953925241260:web:c28b17abc4e8e311e39209"
+};
+
+// Initialize Firebase
+/** @global */
+window.firebaseApp = initializeApp(firebaseConfig);
 
 const app = new App({
   target: document.getElementById('app'),
