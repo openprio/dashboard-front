f22c817176627d7eefe6b0694e3fc421e881cd53
matthijsdam <matthijs0894@hotmail.com>
2024-02-02 15:11:40 +0100

    Added websocket with protobuf

--- src/socket.js
+++ src/socket.js
@@ -1,32 +1,46 @@
 import { writable } from 'svelte/store';
+import * as proto from "protobufjs";
 
-const messageStore = writable('');
+const messageStore = writable();
+let messageType;
 
 const socket = new WebSocket('wss://api.openprio.nl/rt/positions');
+
+proto.load("openprio_pt_position_data.proto", function(err, root) {
+    if (err) {
+        throw err;
+    }
+
+    // Obtain a message type
+    messageType = root.lookupType("LocationMessage");
+});
 
 // Connection opened
 socket.addEventListener('open', function (event) {
     console.log("It's open");
 });
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    if (event.data instanceof Blob) {
-        event.data.text()
-            .then(r => {
-                console.log(r);
-                messageStore.set(r)
-            })
-    }
-});
+socket.onmessage = async function(event) {
+    let arrayBuffer = await event.data.arrayBuffer();
+    try {
+        let result = messageType.decode(new Uint8Array(arrayBuffer));
 
-const sendMessage = (message) => {
-    if (socket.readyState <= 1) {
-        socket.send(message);
+        messageStore.set(result);
+
+        console.log(result);
+    } catch (error) {
+        console.log('Couldn\'t read buffer.');
     }
-}
+    // plotPosition(result);
+};
+
+// const sendMessage = (message) => {
+//     if (socket.readyState <= 1) {
+//         socket.send(message);
+//     }
+// }
 
 export default {
     subscribe: messageStore.subscribe,
-    sendMessage
+    // sendMessage
 }
