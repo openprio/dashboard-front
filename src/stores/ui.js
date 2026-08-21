da08303abc37213aa83239aefa88236ffe7ab8b6
Sven <sven.boor@transbits.nl>
2026-08-21 20:16:28 +0200

    Simplified filter menu

--- src/stores/ui.js
+++ src/stores/ui.js
@@ -1,9 +1,3 @@
 import { writable } from "svelte/store";
 
-export const show_filters = writable(
-  JSON.parse(localStorage.getItem("show_filters") ?? '"true"'),
-);
-
-show_filters.subscribe((value) => {
-  localStorage.setItem("show_filters", JSON.stringify(value));
-});
+export const show_filters = writable(false);
